66c1be2e146b2efa2eea0ec4e4d6f21b9c0673e7
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-02-22 19:20:36 +0200

    cursor style

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // Variables
 const dealer = document.getElementById("dealer");
 const player = document.getElementById("player");
@@ -130,3 +131,41 @@ window.onload = function game_init() {
         }
     })
 };
+// cursor
+var rjs_cursor = document.getElementById("rjs_cursor"); //Getting the cursor
+var body = document.querySelector("body"); //Get the body element
+
+//Functions for showing and hiding the cursor
+//They are referenced the 
+function rjs_show_cursor(e) { //Function to show/hide the cursor
+    if (rjs_cursor.classList.contains('rjs_cursor_hidden')) {
+        rjs_cursor.classList.remove('rjs_cursor_hidden');
+    }
+    rjs_cursor.classList.add('rjs_cursor_visible');
+}
+
+function rjs_hide_cursor(e) {
+    if (rjs_cursor.classList.contains('rjs_cursor_visible')) {
+        rjs_cursor.classList.remove('rjs_cursor_visible');
+    }
+    rjs_cursor.classList.add('rjs_cursor_hidden');
+}
+
+
+function rjs_mousemove(e) { //Function to correctly position the cursor
+    rjs_show_cursor(); //Toggle show/hide
+
+    var rjs_cursor_width = rjs_cursor.offsetWidth * 0.5;
+    var rjs_cursor_height = rjs_cursor.offsetHeight * 0.5;
+
+    var rjs_cursor_x = e.clientX - rjs_cursor_width; //x-coordinate
+    var rjs_cursor_y = e.clientY - rjs_cursor_height; //y-coordinate
+    var rjs_cursor_pos = `translate(${rjs_cursor_x}px, ${rjs_cursor_y}px)`;
+    rjs_cursor.style.transform = rjs_cursor_pos;
+}
+
+
+//Eventlisteners
+
+window.addEventListener('mousemove', rjs_mousemove); //Attach an event listener
+body.addEventListener('mouseleave', rjs_hide_cursor);
